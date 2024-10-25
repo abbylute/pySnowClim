a43061946fb86b07d3f027b92dff5b5270de4c38
Aranildo Lima <aranildo.lima@ec.gc.ca>
2024-10-25 23:56:29 +0000

    changes to remove time dependency of forcing
    - refactored script that calculates albedo to received less parameters.
    
    - refactored script that calculates turbulent fluxes to received less parameters.
    
    - create function to do the final conversion in the outputs of the model
    
    - removed time dependency (`i`) when passing forcings to the functions
    
    - changed name of a few variables to make things more clear
    
    Closes #14

--- src/calcAlbedo.py
+++ src/calcAlbedo.py
@@ -1,8 +1,8 @@
 """
-This module contains functions to calculate snow albedo using different snow models: 
-Essery et al. (2013), the Utah Snow Model (Tarboton), and the VIC model. These models 
-compute the albedo based on snow properties such as snow depth, snow water equivalent 
-(SWE), snow temperature, and snow age, along with environmental conditions like latitude, 
+This module contains functions to calculate snow albedo using different snow models:
+Essery et al. (2013), the Utah Snow Model (Tarboton), and the VIC model. These models
+compute the albedo based on snow properties such as snow depth, snow water equivalent
+(SWE), snow temperature, and snow age, along with environmental conditions like latitude,
 ground albedo, and time of year.
 
 Main Function:
@@ -12,40 +12,35 @@ Main Function:
 Albedo Calculation Models:
 ---------------------------
 1. Essery et al. (2013) Option 1:
-   - Calculates snow albedo based on fresh snow, cold snow, and melting snow conditions 
+   - Calculates snow albedo based on fresh snow, cold snow, and melting snow conditions
      using equations from Essery et al. (2013), with parameter values from Douville et al. (1995).
-   
+
 2. Utah Snow Model (Tarboton):
    - Albedo is calculated using equations adapted from the Utah Energy Balance (UEB) model.
-   - Albedo is a function of snow age, temperature, and new snow depth, taking into account 
+   - Albedo is a function of snow age, temperature, and new snow depth, taking into account
      latitude, month, and day.
 
 3. VIC Model:
-   - The VIC (Variable Infiltration Capacity) model calculates albedo for new snow, cold-aged snow, 
-     and melting snow conditions, using snow depth and snow age. The model was adapted from the 
+   - The VIC (Variable Infiltration Capacity) model calculates albedo for new snow, cold-aged snow,
+     and melting snow conditions, using snow depth and snow age. The model was adapted from the
      VIC snow utility.
 """
 
 import numpy as np
 import constants as const
+from calcInclinationAngle import calc_inclination_angle
 
-def calc_albedo(albedo_option, ground_albedo, max_albedo, last_albedo, new_snow_depth, 
-                     last_snow_depth, new_swe, last_swe, last_snow_temp, lat, 
-                     month, day, snow_age, last_pack_cc, sec_in_ts):
+
+def calc_albedo(parameters, last_albedo, new_snow_depth,
+                last_snow_depth, new_swe, last_swe, last_snow_temp, lat,
+                month, day, snow_age, last_pack_cc, sec_in_ts):
     """
     Calculate the snow albedo based on the selected model option and various snow and environmental parameters.
 
     Parameters:
     -----------
-    albedo_option : int
-        The albedo calculation method to use. Options include:
-        1 = Essery et al. (2013) option 1
-        2 = Utah Snow Model (Tarboton)
-        3 = VIC model
-    ground_albedo : float
-        The albedo of the ground surface.
-    max_albedo : float
-        Maximum snow albedo.
+    parameters: dict
+        A dictionary of model parameters, including 'ground_albedo' and 'albedo_option'.
     last_albedo : ndarray
         Previous timestep snow albedo values.
     new_snow_depth : ndarray
@@ -58,8 +53,8 @@ def calc_albedo(albedo_option, ground_albedo, max_albedo, last_albedo, new_snow_
         SWE from the previous timestep.
     last_snow_temp : ndarray
         Snow temperature from the previous timestep.
-    lat : ndarray
-        Latitude of the snow surface points.
+    lat: np.ndarray
+        Array of latitudes.
     month : int
         Month of the current timestep (1-12).
     day : int
@@ -78,32 +73,35 @@ def calc_albedo(albedo_option, ground_albedo, max_albedo, last_albedo, new_snow_
     snow_age : ndarray
         Updated snow age for each grid point.
     """
-    
-    if albedo_option == 1:  # Essery et al. (2013) option 1
-        albedo = _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe, max_albedo, ground_albedo, sec_in_ts)
-        
-    elif albedo_option == 2:  # Utah Snow Model (Tarboton)
-        albedo, snow_age = _calc_albedo_tarboton(last_snow_temp + 273.15, snow_age, new_snow_depth, lat, month, day, last_swe, ground_albedo, max_albedo, sec_in_ts)
-        
-    elif albedo_option == 3:  # VIC model
-        albedo, snow_age = _calc_albedo_vic(new_snow_depth, snow_age, ground_albedo, last_snow_depth, last_albedo, max_albedo, sec_in_ts, last_pack_cc)
+
+    if parameters['albedo_option'] == 1:
+        albedo = _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe,
+                                          parameters, sec_in_ts)
+    elif parameters['albedo_option'] == 2:
+        albedo, snow_age = _calc_albedo_tarboton(last_snow_temp + const.K_2_C, snow_age,
+                                                 new_snow_depth, lat, month, day, last_swe,
+                                                 parameters, sec_in_ts)
+    elif parameters['albedo_option'] == 3:
+        albedo, snow_age = _calc_albedo_vic(new_snow_depth, snow_age, parameters,
+                                            last_snow_depth, last_albedo, sec_in_ts,
+                                            last_pack_cc)
 
     # Adjust albedo if total snow depth is low (< 0.1 m)
     z = last_snow_depth
     f = z < 0.1
     r = (1 - z[f] / 0.1) * np.exp(-z[f] / 0.2)
     r = np.minimum(r, 1)
-    albedo[f] = r * ground_albedo + (1 - r) * albedo[f]
+    albedo[f] = r * parameters['ground_albedo'] + (1 - r) * albedo[f]
 
     # Ensure albedo is real and within range
     albedo = np.real(albedo)
-    albedo = np.clip(albedo, 0, max_albedo)
+    albedo = np.clip(albedo, 0, parameters['max_albedo'])
 
     return albedo, snow_age
 
 
-def _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe, max_albedo, 
-                             ground_albedo, sec_in_ts):
+def _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe, parameters,
+                             sec_in_ts):
     """
     Calculate snow albedo based on Essery et al. (2013) option 1.
 
@@ -126,11 +124,12 @@ def _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe, max
 
     # No snow on the ground
     b = last_swe == 0
-    albedo[b] = ground_albedo
+    albedo[b] = parameters['ground_albedo']
 
     # Fresh snow
     b = new_swe > 0
-    albedo[b] = last_albedo[b] + (max_albedo - last_albedo[b]) * ((Sf[b] * dt) / So)
+    albedo[b] = last_albedo[b] + (parameters['max_albedo'] -
+                                  last_albedo[b]) * ((Sf[b] * dt) / So)
 
     # Cold snow
     b = (last_swe > 0) & (new_swe == 0) & (last_snow_temp < -0.5)
@@ -140,13 +139,13 @@ def _calc_albedo_essery_opt1(last_albedo, last_snow_temp, new_swe, last_swe, max
     b = (last_swe > 0) & (new_swe == 0) & (last_snow_temp >= -0.5)
     albedo[b] = (last_albedo[b] - min_albedo) * np.exp(-dt / Tm) + min_albedo
 
-    albedo = np.clip(albedo, min_albedo, max_albedo)
+    albedo = np.clip(albedo, min_albedo, parameters['max_albedo'])
 
     return albedo
 
 
-def _calc_albedo_vic(new_snow_depth, snow_age, ground_albedo, last_snow_depth, 
-                     last_albedo, max_albedo, sec_in_ts, last_pack_cc):
+def _calc_albedo_vic(new_snow_depth, snow_age, parameters, last_snow_depth,
+                     last_albedo, sec_in_ts, last_pack_cc):
     """
     Calculate snow albedo based on the VIC model.
 
@@ -159,7 +158,7 @@ def _calc_albedo_vic(new_snow_depth, snow_age, ground_albedo, last_snow_depth,
     snow_age : ndarray
         Updated snow age.
     """
-    SNOW_NEW_SNOW_ALB = max_albedo
+    SNOW_NEW_SNOW_ALB = parameters['max_albedo']
     SNOW_ALB_ACCUM_A = 0.94
     SNOW_ALB_ACCUM_B = 0.58
     SNOW_ALB_THAW_A = 0.82
@@ -179,54 +178,105 @@ def _calc_albedo_vic(new_snow_depth, snow_age, ground_albedo, last_snow_depth,
 
     # Cold snow
     c = b & (last_pack_cc < 0)
-    albedo[c] = SNOW_NEW_SNOW_ALB * SNOW_ALB_ACCUM_A ** ((snow_age[c] / sec_per_day) ** SNOW_ALB_ACCUM_B)
+    albedo[c] = SNOW_NEW_SNOW_ALB * \
+        SNOW_ALB_ACCUM_A ** ((snow_age[c] / sec_per_day) ** SNOW_ALB_ACCUM_B)
 
     # Melting snow
     c = b & (last_pack_cc == 0)
-    albedo[c] = SNOW_NEW_SNOW_ALB * SNOW_ALB_THAW_A ** ((snow_age[c] / sec_per_day) ** SNOW_ALB_THAW_B)
+    albedo[c] = SNOW_NEW_SNOW_ALB * \
+        SNOW_ALB_THAW_A ** ((snow_age[c] / sec_per_day) ** SNOW_ALB_THAW_B)
 
     # No snow case
     b = last_snow_depth == 0
     snow_age[b] = 0
-    albedo[b] = ground_albedo
+    albedo[b] = parameters['ground_albedo']
 
     return albedo, snow_age
 
 
-def _calc_albedo_tarboton(last_snow_temp, snow_age, new_snow_depth, lat, month, day, 
-                          last_swe, ground_albedo, max_albedo, sec_in_ts):
+def _calc_albedo_tarboton(lastsnowtemp, snowage, newsnowdepth, lat, month, day, lastswe,
+                          parameters, sec_in_ts):
     """
-    Calculate snow albedo based on the Utah Snow Model (Tarboton).
+    Calculate albedo and snowage based on Tarboton model parameters.
 
-    Parameters are similar to those in `calc_albedo`.
+    Parameters:
+    -----------
+    - lastsnowtemp: np.ndarray
+        Array of last snow temperature.
+    - snowage: np.ndarray
+        Array of snow age values to be updated.
+    - newsnowdepth: np.ndarray
+        Array of new snow depths.
+    - lat: np.ndarray
+        Array of latitudes.
+    - month: int
+        Current month (for inclination angle calculation).
+    - day: int
+        Current day (for inclination angle calculation).
+    - lastswe: np.ndarray
+        Array of last snow water equivalent.
+    - lat: np.ndarray
+        Array of latitudes.
+    - sec_in_ts: float
+        Seconds in the time step.
 
     Returns:
     --------
-    albedo : ndarray
-        Calculated snow albedo.
-    snow_age : ndarray
-        Updated snow age.
+    - albedo: np.ndarray
+        Calculated albedo values.
+    - snowage: np.ndarray
+        Updated snow age values.
     """
+    # Constants
     Cv = 0.2
     Cir = 0.5
-
     albedo_iro = 0.65
-    albedo_vo = 2 * max_albedo - albedo_iro
+    albedo_vo = 2 * parameters['max_albedo'] - albedo_iro
+
     if albedo_vo > 1:
         d = albedo_vo - 1
         albedo_iro += d
         albedo_vo = 1
 
-    albedo = np.zeros_like(last_swe)
+    # Initialize albedo array
+    albedo = np.zeros_like(lastsnowtemp)
 
-    # New snow depth > 0.01m
-    b = new_snow_depth > 0.01
+    # For new snow depths > 0.01 m
+    b = newsnowdepth > 0.01
     albedo[b] = albedo_vo / 2 + albedo_iro / 2
-    snow_age[b] = 0
+    snowage[b] = 0
 
-    # No snow case
-    b = last_swe == 0
-    albedo[b] = ground_albedo
-    snow_age[b] = 0
+    inc = calc_inclination_angle(lat[b], month, day) * np.pi / 180
+    c = np.cos(inc) < 0.5
+    fpsi = 0.5 * (3. / (1 + 4 * np.cos(inc[c])) - 1)
+    extra = np.zeros_like(inc)
+    extra[c] = 0.2 * fpsi * (1 - albedo_vo) + 0.2 * fpsi * (1 - albedo_iro)
+    albedo[b] += extra
 
-    return albedo, snow_age
+    # For new snow depths <= 0.01 m (albedo is a function of snow age)
+    b = ~b
+    r1 = np.exp(5000 * (1 / 273.16 - 1. / lastsnowtemp[b]))
+    r2 = np.minimum(r1**10, 1)
+    r3 = 0.03
+
+    inc_age = (r1 + r2 + r3) / 1e6 * sec_in_ts
+    snowage[b] += inc_age
+    Fage = snowage[b] / (1 + snowage[b])
+
+    albedo_1 = (1 - Cv * Fage) * albedo_vo
+    albedo_2 = (1 - Cir * Fage) * albedo_iro
+    albedo[b] = albedo_1 / 2 + albedo_2 / 2
+
+    inc = calc_inclination_angle(lat[b], month, day) * np.pi / 180
+    c = np.cos(inc) < 0.5
+    fpsi = 0.5 * (3. / (1 + 4 * np.cos(inc[c])) - 1)
+    extra = np.zeros_like(inc)
+    extra[c] = 0.2 * fpsi * (1 - albedo_1[c]) + 0.2 * fpsi * (1 - albedo_2[c])
+    albedo[b] += extra
+
+    # For no snow case
+    b = lastswe == 0
+    albedo[b] = parameters['ground_albedo']
+    snowage[b] = 0
+
+    return albedo, snowage
--- src/calcTurbulentFluxes.py
+++ src/calcTurbulentFluxes.py
@@ -8,106 +8,102 @@ from calcLatHeatVap import calculate_lat_heat_vap
 from calcLatHeatSub import calculate_lat_heat_sub
 import constants as const
 
-def calc_turbulent_fluxes(stability, windHt, z_0, tempHt, z_h, vs, lastsnowtemp, tavg, 
-                          psfc, huss, E0_value, E0_app, E0_stable, sec_in_ts):
 
+def calc_turbulent_fluxes(parameters,  tempHt,  vs, lastsnowtemp, tavg,
+                          psfc, huss, sec_in_ts):
     """
-    Calculate turbulent fluxes of heat and mass (evaporation/sublimation) using 
+    Calculate turbulent fluxes of heat and mass (evaporation/sublimation) using
     Richardson number parameterization based on Essery et al. (2013).
-    
+
     Parameters:
-    stability: Boolean indicating whether stability parameterization is used
-    windHt: Wind measurement height (m)
-    z_0: Roughness length (m)
+    parameters: dictionary with the default parameters to be used.
     tempHt: Temperature measurement height (m)
-    z_h: Reference height for temperature profile (m)
     vs: Wind speed (m/s)
     lastsnowtemp: Snow surface temperature (C)
     tavg: Air temperature (C)
-    Ca: Specific heat of air (kJ/kg/K)
     psfc: Surface pressure (hPa)
     huss: Specific humidity (kg/kg)
-    E0_value: Windless exchange coefficient (W/m2/K)
-    E0_app: Option for applying windless exchange (1 or 2)
-    E0_stable: Stability option for windless exchange (1 or 2)
     sec_in_ts: Seconds in the current timestep
-    
+
     Returns:
     H: Sensible heat flux (kJ/m2/timestep)
     E: Latent heat flux (kg/m2/timestep)
     EV: Energy flux due to evaporation or sublimation (kJ/m2/timestep)
     """
-    
     # Constants
     R = 287  # gas constant for dry air (J K-1 kg-1)
     g = 9.80616  # gravitational acceleration (m/s^2)
     c = 5  # stability constant from Essery et al., (2013), table 6
-    E0 = E0_value / 1000  # Convert E0 to kJ/m2/K/s
-    
+    E0 = parameters['E0_value'] / 1000  # Convert E0 to kJ/m2/K/s
+
     # Convert air pressure from hPa to Pa
     psfcpa = psfc * 100
-    
+
     # Calculate air density (kg/m^3)
-    pa = psfcpa / (R * (tavg + 273.15))
-    
+    pa = psfcpa / (R * (tavg + const.K_2_C))
+
     # Calculate vapor densities (kg/m^3)
     rhoa = huss
     rhos = calculate_specific_humidity(lastsnowtemp, psfc)
-    
+
     # Exchange coefficient for neutral conditions (CHN)
-    CHN = const.K**2 / (np.log(windHt / z_0) * np.log(tempHt / z_h))
-    
-    if stability:
+    CHN = const.K**2 / \
+        (np.log(parameters['windHt'] / parameters['z_0'])
+         * np.log(tempHt / parameters['z_h']))
+
+    if parameters['stability']:
         # Calculate the bulk Richardson number
-        Rib = (g * windHt * (tavg - lastsnowtemp)) / ((tavg + 273.15) * vs**2)
-        
+        Rib = (g * parameters['windHt'] * (tavg - lastsnowtemp)) / \
+            ((tavg + const.K_2_C) * vs**2)
+
         # Calculate FH as a function of Rib
         FH = np.full_like(tavg, np.nan)
         # For unstable case
-        FH[Rib < 0] = 1 - ((3 * c * Rib[Rib < 0]) / (1 + 3 * c**2 * CHN * (-Rib[Rib < 0] * windHt / z_0)**0.5))
+        FH[Rib < 0] = 1 - ((3 * c * Rib[Rib < 0]) / (1 + 3 * c**2 * CHN *
+                           (-Rib[Rib < 0] * parameters['windHt'] / parameters['z_0'])**0.5))
         # For neutral case
         FH[Rib == 0] = 1
         # For stable case
         FH[Rib > 0] = (1 + ((2 * c * Rib[Rib > 0]) / (1 + Rib[Rib > 0])**0.5))**-1
-        
+
         # Calculate exchange coefficient CH
         CH = FH * CHN
     else:
         CH = CHN
-    
+
     # Latent heat of vaporization and sublimation
     LatHeatVap = calculate_lat_heat_vap(lastsnowtemp)  # kJ/kg
     LatHeatSub = calculate_lat_heat_sub(lastsnowtemp)  # kJ/kg
-    
+
     # Windless exchange coefficient
     Ex = np.zeros_like(vs)
-    if E0_stable == 1:
+    if parameters['E0_stable'] == 1:
         Ex = E0
-    elif E0_stable == 2:
+    elif parameters['E0_stable'] == 2:
         Ex[Rib > 0] = E0
-    
+
     # Sensible heat flux (H)
     H = -(pa * const.CA * CH * vs + Ex) * (lastsnowtemp - tavg)
-    
+
     # Latent heat flux (E)
-    if E0_app == 1:
+    if parameters['E0_app'] == 1:
         E = -(pa * CH * vs) * (rhos - rhoa)  # Mass flux kg/m2/s
-    elif E0_app == 2:
+    elif ['E0_app'] == 2:
         E = -(pa * CH * vs + Ex) * (rhos - rhoa)  # Mass flux kg/m2/s
-    
+
     # Evaporation and sublimation energy flux (EV)
     Evap = E * LatHeatVap
     Esub = E * LatHeatSub
     EV = np.where(lastsnowtemp >= 0, Evap, Esub)
-    
+
     # Convert from per second to per timestep
     H *= sec_in_ts  # kJ/m2/s
     E *= sec_in_ts  # kg/m2/s
     EV *= sec_in_ts  # kJ/m2/s
-    
+
     # Avoid NaNs for zero wind speed
     H[vs == 0] = 0
     E[vs == 0] = 0
     EV[vs == 0] = 0
-    
+
     return H, E, EV
