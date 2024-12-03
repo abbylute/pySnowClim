1e6d54274a7ef9000b1c06a83f89cf7c6cdf6565
Aranildo Lima <aranildo.lima@ec.gc.ca>
2024-12-03 00:39:20 +0000

    Several changes to add snowpack methods
    - Added tolerance to compare to zero.
    
    - changed comparison of matrix to `np.logical...`.
    
    - Refactored functions to use snowpack methods.
    
    - Updated snowpack class.
    
    - Updated readme file.
    
    - Updated script interface to run the code.
    
    - Closes #42

--- run_main.py
+++ run_main.py
@@ -1,17 +1,41 @@
 """Main script to run pySnowClim."""
 import os
 import sys
+import argparse
+
 # Add the 'code' directory to the Python path
 sys.path.append('src/')
+
+#import pdb; pdb.set_trace()
 
 from runsnowclim_model import run_model
 
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(
+        description='pySnowClim model',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('path',
+                        help='Path where model inputs are located',
+                        default='data/',
+                        nargs='?')
+
+    parser.add_argument('output',
+                        help='Path where model outputs will be saved',
+                        default='outputs/',
+                        nargs='?')
+
+    parser.add_argument('parameters',
+                        help='Path where the parameters file is located',
+                        default=None,
+                        nargs='?')
+
+
+    args = vars(parser.parse_args())
+
+
     print('Starting pySnowClim model...')
-    
-    model_output = run_model('data/', 'data/parameter.mat', 'outputs/')
+    model_output = run_model(args['path'], args['parameters'], args['output'])
     print('pySnowClim model finished!')
-
-
